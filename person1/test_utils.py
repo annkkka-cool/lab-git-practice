14fe54a6d0394b93e5907e87fd0b3ed6c883a9d1
annkkka-cool <bulaeva.anna2006@mail.ru>
2026-05-27 21:12:54 +0300

    Update test_utils.py

--- person1/test_utils.py
+++ person1/test_utils.py
@@ -1,10 +1,14 @@
 import unittest
-from utils import add
+from utils import add, multiply
 
 class TestUtils(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
+    
+    def test_multiply(self):
+        self.assertEqual(multiply(2, 3), 6)
+        self.assertEqual(multiply(-2, 3), -6)
 
 if __name__ == '__main__':
     unittest.main()
